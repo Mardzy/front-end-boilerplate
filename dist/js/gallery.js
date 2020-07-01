2c8aa8b3de1a5ee21a0e4062b3ac467145729f07
Mardzy <mardlingp@gmail.com>
2020-07-01 14:09:19 +0200

    Update backup image

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -106,7 +106,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getElementByClass\", function() { return getElementByClass; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"backupImage\", function() { return backupImage; });\nvar getElementByClass = function getElementByClass(className) {\n  return document.querySelector(className);\n};\nvar backupImage = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pngkit.com%2Fpng%2Ffull%2F49-498166_yoda-silhouette-png-star-wars-topps-funny.png&imgrefurl=https%3A%2F%2Fwww.pngkit.com%2Fbigpic%2Fu2q8e6a9u2t4w7u2%2F&tbnid=VSO2dLaOowj30M&vet=10CBUQxiAoAmoXChMI-NLj7oKp6gIVAAAAAB0AAAAAEAg..i&docid=fNk0lz70kC3nlM&w=692&h=1052&itg=1&q=default%20star%20wars%20image&ved=0CBUQxiAoAmoXChMI-NLj7oKp6gIVAAAAAB0AAAAAEAg';\n\n//# sourceURL=webpack:///./src/js/components/helpers/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getElementByClass\", function() { return getElementByClass; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"backupImage\", function() { return backupImage; });\nvar getElementByClass = function getElementByClass(className) {\n  return document.querySelector(className);\n};\nvar backupImage = 'https://www.pngkit.com/png/detail/49-498166_yoda-silhouette-png-star-wars-topps-funny.png';\n\n//# sourceURL=webpack:///./src/js/components/helpers/index.js?");
 
 /***/ })
 
